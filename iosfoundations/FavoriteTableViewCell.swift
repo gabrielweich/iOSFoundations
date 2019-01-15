5ded45c3987248dd421b532c3e456e280bd90e27
Henrique Reis Kops <henrique@10-41-40-14.0101.epoa.br.iosda.org>
2019-01-15 18:04:52 -0200

    favorites table view

---
+++ iosfoundations/FavoriteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteTableViewCell.swift
+//  iosfoundations
+//
+//  Created by Henrique Reis Kops on 15/01/19.
+//  Copyright © 2019 Gabriel Weich. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- iosfoundations/FavoritesViewController.swift
+++ iosfoundations/FavoritesViewController.swift
@@ -1,25 +1,22 @@
 //
-//  ListViewController.swift
+//  FavoritesViewController.swift
 //  iosfoundations
 //
-//  Created by Gabriel Weich on 15/01/19.
+//  Created by Henrique Reis Kops on 15/01/19.
 //  Copyright © 2019 Gabriel Weich. All rights reserved.
 //
 
 import UIKit
 
-class ListViewController: UIViewController {
-    
-    var category:String=""
+class FavoritesViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var titleOut: UINavigationItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleOut.title = category
         tableView.dataSource = self
         tableView.delegate = self
+
         // Do any additional setup after loading the view.
     }
     
@@ -33,21 +30,20 @@ class ListViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
 
 }
 
-
-extension ListViewController: UITableViewDataSource, UITableViewDelegate{
+extension FavoritesViewController: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:BusinessItemTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell") as! BusinessItemTableViewCell
         
-        cell.title?.text="gesiel"
+        let cell:FavoriteTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell") as!FavoriteTableViewCell
+
         return cell
     }
-    
     
 }
