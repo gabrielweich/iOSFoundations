69fb355d9e546672b0c4bd2fa81dbe9d2db150d5
Gabriel Weich <gabriel@10-41-48-253.0200.epoa.br.iosda.org>
2019-01-15 16:05:43 -0200

    gesiel

---
+++ iosfoundations/BusinessItem.swift
@@ -0,0 +1,16 @@
+//
+//  BusinessItem.swift
+//  iosfoundations
+//
+//  Created by Gabriel Weich on 15/01/19.
+//  Copyright © 2019 Gabriel Weich. All rights reserved.
+//
+
+import Foundation
+
+
+struct BusinessItem {
+    let title: String
+    let description: String
+    var favorited: Bool
+}
