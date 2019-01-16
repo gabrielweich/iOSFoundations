7f87bab0a42aa90f093e39b42aa412139eb254b5
Gabriel Weich <gabriel@10-41-40-14.0101.epoa.br.iosda.org>
2019-01-16 15:35:51 -0200

    business item gesiel

---
+++ iosfoundations/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  iosfoundations
+//
+//  Created by Gabriel Weich on 16/01/19.
+//  Copyright © 2019 Gabriel Weich. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    var key: String
+    var value: String
+}
